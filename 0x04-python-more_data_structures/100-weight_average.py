b8aa55c874090893b30245feb147b85d2dbb1b3f
rvkuu <preciousubi.sam@gmail.com>
2023-03-17 13:12:55 -0700

    More Data Structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def weight_average(my_list)=[]):
+def weight_average(my_list=[]):
     if not my_list:
         return 0
 
@@ -10,4 +10,4 @@ def weight_average(my_list)=[]):
         num += tup[0] * tup[1]
         den += tup[1]
 
-        return (num / den)
+    return (num / den)
